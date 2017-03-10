fb675daf23ab855b43247a30b4d618573d679219
courtney <coa.mcallister@gmail.com>
2017-03-09 20:14:56 -0500

    connected to mongo database using mongoose. created a schema and model for Foods that will be stored in the database. implemented get /foods, /foods/:id, and /foods/category/:categoryName and sends the information to the requesting party

---
+++ index.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var server = express();
+var mongoose = require('mongoose');
+var port = process.env.PORT || 8080;
+var mongoURI = process.env.MONGOURI || require('./secrets').mongoURI;
+
+//connect to the database
+mongoose.connect(mongoURI);
+
+//create schema
+var foodSchema = mongoose.Schema({
+  price: Number,
+  category: String,
+  isGlutenFree: Boolean,
+  calories: Number
+});
+
+//create the Mongoose model
+var Food = mongoose.model('Food', foodSchema);
+
+//testing
+// var chocolateBar = new Food({
+//   price: 3,
+//   category: 'candy',
+//   isGlutenFree: true,
+//   calories: 200
+// })
+// chocolateBar.save(function(err, data){
+//   if(err){
+//     console.log(err);
+//   } else{
+//     console.log(data);
+//   }
+// });
+
+//GET /foods
+server.get('/foods', function(req, res){
+  Food.find({}, function(err, documents){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        foods: documents
+      });
+    }
+  });
+});
+
+//GET /foods/:id
+server.get('/foods/:id', function(req, res){
+  Food.find({_id: req.params.id}, function(err, documents){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        foods: documents
+      });
+    }
+  });
+});
+
+//GET /foods/category/:categoryName
+server.get('/foods/category/:categoryName', function(req, res){
+  Food.find({category: req.params.categoryName}, function(err, documents){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        foods: documents
+      });
+    }
+  });
+});
+
+server.listen(port, function(){
+  console.log('Now listening on', port);
+})
